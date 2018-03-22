4a91a7aab80b6c81f58593dc6de93f0a3dc5516a
Alexander Spanos <alexisspa9@gmail.com>
2018-03-22 09:18:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-if array.size == 1
-  array[0]
-elsif
-
-else  
-array[0..-2].join(", ") + " and " + array[-1]
+  if array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif 2 < array.length
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ") 
 end
